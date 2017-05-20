644190385fea298387778caed41ab321baedb91f
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2017-05-20 14:20:39 +0200

    Introducing app-layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,7 +42,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // Scroll page to top and expand header
   app.scrollPageToTop = function() {
-    app.$.headerPanelMain.scrollToTop(true);
+    try {
+      app.$.appLayout.scroll(0, 0);
+    }
+    catch (e) {}
   };
 
   // go to the base URL
